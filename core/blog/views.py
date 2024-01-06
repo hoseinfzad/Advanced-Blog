d6eec25e043f1674f1128c644585490a919d73a4
hoseinfzad <hosein.fereydonizad@gmail.com>
2024-01-06 17:47:15 +0330

    add template directory in base dir adn statics in settings and urls

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView, RedirectView
 
 # Create your views here.
+class Index(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['message'] = 'Hello World!'
+        return context
